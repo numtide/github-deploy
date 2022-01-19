903d92c49a63eb67cea5631bc996773924a3f936
Jean-François Roche <jfroche@affinitic.be>
2022-01-19 21:25:19 +0100

    feat: switch to flake, add fmt, lint

--- default.nix
+++ default.nix
@@ -1,2 +1,38 @@
-{ pkgs ? import (import ./nix/sources.nix).nixpkgs { } }:
-pkgs.callPackage ./github-deploy.nix { }
+{ system ? builtins.currentSystem
+, inputs ? import ./flake.lock.nix { }
+, nixpkgs ? import inputs.nixpkgs {
+    inherit system;
+    # Makes the config pure as well. See <nixpkgs>/top-level/impure.nix:
+    config = { };
+    overlays = [ ];
+  }
+, buildGoPackage ? nixpkgs.buildGoPackage
+}:
+let
+  versionFile = builtins.readFile ./version.go;
+  versionMatch = builtins.match ".*\"([0-9]+\\.[0-9]+\\.[0-9]+)\".*" versionFile;
+  version = builtins.head versionMatch;
+  github-deploy = buildGoPackage
+    rec {
+      name = "github-deploy-${version}";
+      goPackagePath = "github.com/zimbatm/github-deploy";
+      src = nixpkgs.lib.cleanSource ./.;
+      # FIXME: for some reason the go reference rewrites are failing
+      allowGoReference = true;
+    };
+in
+{
+  inherit github-deploy;
+  defaultPackage = github-deploy;
+  devShell = nixpkgs.mkShell {
+    buildInputs = with nixpkgs; [
+      go
+      gofumpt
+      golangci-lint
+      gopls
+      nixpkgs-fmt
+      just
+      treefmt
+    ];
+  };
+}
