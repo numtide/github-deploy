cf6139103e393a0dcdf5da2e591dc161704371cb
Jean-François Roche <jfroche@affinitic.be>
2022-01-19 20:38:34 +0100

    feat(cleanup): filter deployments based on a given PR list
    Add option to cleanup deployments related to specific PRs instead of
    cleaning up all deployments related to closed PRs.

--- command/cleanup.go
+++ command/cleanup.go
@@ -2,6 +2,7 @@ package command
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"log"
 	"os"
@@ -15,45 +16,43 @@ import (
 func CmdCleanup(c *cli.Context) (err error) {
 	owner, repo := githubSlug(c)
 	listScript := c.String("list-script")
+	selectedPullRequest := c.StringSlice("pull-request")
+	if listScript == "" && len(selectedPullRequest) == 0 {
+		return errors.New("`--list-script` or `--pull-request` missing. You have to define one or the other to select the PRs to cleanup !")
+	}
 	ctx := context.Background()
 	gh := githubClient(ctx, c)
 
-	// Get the list of temporary deployments
-	var stdout strings.Builder
-	cmd := exec.Command(listScript)
-	cmd.Stdout = &stdout
-	err = cmd.Run()
-	if err != nil {
-		return err
-	}
-	var deployed []string
-	for _, line := range strings.Split(stdout.String(), "\n") {
-		if line == "" {
-			continue
+	var toUndeploy []string
+	if len(selectedPullRequest) > 0 {
+		// undeploy only selected pull requests
+		toUndeploy = selectedPullRequest
+	} else {
+		// undeploy all closed pull requests
+		var deployed []string
+		deployed, err = listDeployedPullRequests(listScript)
+		if err != nil {
+			return err
 		}
 
-		deployed = append(deployed, line)
-	}
-	log.Println("deployed:", deployed)
+		// Get the list of open PRs
+		prs, _, err := gh.PullRequests.List(ctx, owner, repo, &github.PullRequestListOptions{
+			State: "open",
+		})
+		if err != nil {
+			return err
+		}
+		openPRs := make([]string, len(prs))
+		for i, pr := range prs {
+			openPRs[i] = fmt.Sprintf("pr-%d", *pr.Number)
+		}
+		log.Println("open PRs:", openPRs)
 
-	// Get the list of open PRs
-	prs, _, err := gh.PullRequests.List(ctx, owner, repo, &github.PullRequestListOptions{
-		State: "open",
-	})
-	if err != nil {
-		return err
-	}
-	openPRs := make([]string, len(prs))
-	for i, pr := range prs {
-		openPRs[i] = fmt.Sprintf("pr-%d", *pr.Number)
-	}
-	log.Println("open PRs:", openPRs)
-
-	// Now get a list of all the deployed PRs that are not open
-	var toUndeploy []string
-	for _, name := range deployed {
-		if !contains(name, openPRs) {
-			toUndeploy = append(toUndeploy, name)
+		// Now get a list of all the deployed PRs that are not open
+		for _, name := range deployed {
+			if !contains(name, openPRs) {
+				toUndeploy = append(toUndeploy, name)
+			}
 		}
 	}
 	log.Println("to undeploy:", toUndeploy)
@@ -79,4 +78,25 @@ func contains(item string, list []string) bool {
 		}
 	}
 	return false
+}
+
+// Get the list of deployed Pull request based on given script
+func listDeployedPullRequests(listScript string) ([]string, error) {
+	var stdout strings.Builder
+	cmd := exec.Command(listScript)
+	cmd.Stdout = &stdout
+	err := cmd.Run()
+	if err != nil {
+		return nil, err
+	}
+	var deployed []string
+	for _, line := range strings.Split(stdout.String(), "\n") {
+		if line == "" {
+			continue
+		}
+
+		deployed = append(deployed, line)
+	}
+	log.Println("deployed:", deployed)
+	return deployed, nil
 }
