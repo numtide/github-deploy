1acfe0374c49bb8ed2c6d701b3ba8f35c6c941a0
r-vdp <ramses@well-founded.dev>
2024-10-28 15:43:18 +0100

    Remove unused flake-compat input

--- flake.nix
+++ flake.nix
@@ -1,14 +1,10 @@
 {
   inputs = {
-    flake-compat = {
-      url = "github:edolstra/flake-compat";
-      flake = false;
-    };
     nixpkgs.url = "github:nixos/nixpkgs/release-23.05";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils, flake-compat }:
+  outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
       let
         pkgs = import nixpkgs { inherit system; };
